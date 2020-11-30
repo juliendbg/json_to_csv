c956ef1bc528baf963929c3f348f6866ffcfc600
Julien Delbourgo <j.delbourgo@gmail.com>
2020-11-30 15:50:04 +0100

    Add provided sample files

--- spec/json_to_csv_spec.rb
+++ spec/json_to_csv_spec.rb
@@ -5,7 +5,17 @@ RSpec.describe JsonToCsv do
     expect(JsonToCsv::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  it 'correctly converts the example file' do
+    input_path = File.join(File.dirname(__FILE__), 'files/users.json')
+    output_path = File.join(File.dirname(__FILE__), 'files/users.csv')
+
+    expected_result = File.open(output_path, 'r', &:read)
+
+    Tempfile.open(%w[json_to_csv .csv]) do |f|
+      described_class.convert(input_path, f.path)
+      f.rewind
+
+      expect(f.read).to eq(expected_result)
+    end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'bundler/setup'
+require 'tempfile'
 require 'json_to_csv'
 
 RSpec.configure do |config|
